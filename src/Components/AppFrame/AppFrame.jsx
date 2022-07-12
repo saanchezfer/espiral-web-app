b10bc5e6c37d5e82af509cf3f08b4b20e3db8fe0
Fernando Sanchez <saanchezfer8@gmail.com>
2022-07-12 14:00:41 -0300

    logre centrar!!!!!

--- src/Components/AppFrame/AppFrame.jsx
+++ src/Components/AppFrame/AppFrame.jsx
@@ -17,7 +17,7 @@ const AppFrame = ({ children }) => {
             Asociacion Espiral
           </Typography>
           <Grid style={{ marginLeft: "auto" }}>
-            <IconButton style={{ color: "fff" }}>
+            <IconButton style={{}}>
               <Link href="https://www.instagram.com/asociacionespiral">
                 <CgInstagram />
               </Link>
@@ -30,9 +30,7 @@ const AppFrame = ({ children }) => {
           </Grid>
         </Toolbar>
       </AppBar>
-      <Grid item xs={12} sm={11} md={10} lg={8}>
-        {children}
-      </Grid>
+      <Grid item>{children}</Grid>
     </Grid>
   );
 };
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import AppFrame from "../Components/AppFrame/AppFrame";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 const MainPage = () => {
   return (
     <AppFrame>
-      <Typography>Primer test de la app</Typography>
+      <Grid container justifyContent="center">
+        <Grid item xs={6} justifyItems="center">
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            et reiciendis ipsa quia dolor! Magnam vero nulla tempora quidem
+            architecto. Corrupti est temporibus veritatis laudantium, reiciendis
+            dignissimos voluptatibus hic cupiditate!
+          </Typography>
+        </Grid>
+      </Grid>
     </AppFrame>
   );
 };
